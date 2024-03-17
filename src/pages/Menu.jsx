97c41db73dde4e8b90bca6726c1c9e6d54c8b855
Lay Cipriano <laysa.altheman@lexp.com.br>
2024-03-17 05:51:45 -0300

    organizando rotas

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -5,6 +5,7 @@ import xProfissional from '../assets/expProfissionais.png'
 import goals from '../assets/metas.png'
 import skills from '../assets/skills.png'
 import './Menu.css'
+import { Link } from 'react-router-dom'
 
 
 function Menu() {
@@ -12,34 +13,44 @@ function Menu() {
 
         <div className="menu">
             <div className="header">
-                <img className='logotipo' src={logo} alt='Logotipo Curriculum Generator'></img>
-                <h1 className='title'>Free Curriculum<br/> Generator</h1>
+                 <img className='logotipo' src={logo} alt='Logotipo Curriculum Generator'></img>
+                 <h1 className='title'>Free Curriculum<br/> Generator</h1>
             </div>
 
             <div className="menuItems">
                 <div className="dadosPessoais">
+                <Link to={'/'}>
                     <img className='iconMenu' src={dadosPessoais} alt="ícone do menu, dados pessoais" />
                     <span>Dados Pessoais</span>
+                </Link>
                 </div>
 
                 <div className="educacao">
-                    <img className='iconMenu' src={educacao} alt="ícone do menu, educação" />
-                    <span>Educação</span>
+                    <Link to={'/Educacao'}>
+                        <img className='iconMenu' src={educacao} alt="ícone do menu, educação" />
+                        <span>Educação</span>
+                    </Link>
                 </div>
 
                 <div className="xProfissional">
-                    <img className='iconMenu' src={xProfissional} alt="ícone do menu, experiências profissionais" />
-                    <span>Experiências Profissionais</span>
+                    <Link to={'/xProfissionais'}>
+                        <img className='iconMenu' src={xProfissional} alt="ícone do menu, experiências profissionais" />
+                        <span>Experiências Profissionais</span>
+                    </Link>
                 </div>
 
                 <div className="goals">
-                    <img className='iconMenu' src={goals} alt="ícone do menu, metas profissionais" />
-                    <span>Metas Profissionais</span>
+                    <Link to={'/MetasProfissionais'}>
+                        <img className='iconMenu' src={goals} alt="ícone do menu, metas profissionais" />
+                        <span>Metas Profissionais</span>
+                    </Link>
                 </div>
 
                 <div className="skills">
-                    <img className='iconMenu' src={skills} alt="ícone do menu, habilidades" />
-                    <span>Habilidades</span>
+                    <Link to={'/Habilidades'}>
+                        <img className='iconMenu' src={skills} alt="ícone do menu, habilidades" />
+                        <span>Habilidades</span>
+                    </Link>
                 </div>
             </div>
         </div>
